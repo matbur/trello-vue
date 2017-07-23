aedd5ad4b7b2b4c1156adfe746e27cbb2df2308b
matbur95 <mateusz@burniak.pl>
2017-07-23 15:44:33 +0200

    Move default data to data.js

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,7 @@
 
 <script>
   import Category from './components/Category.vue'
-
-  let store = {
-    tasks: [
-      {id: 0, text: 'task0', category: 0},
-      {id: 1, text: 'task1', category: 0},
-      {id: 2, text: 'task2', category: 1},
-      {id: 3, text: 'task3', category: 2},
-    ],
-    categories: [
-      {id: 0, name: 'backlog'},
-      {id: 1, name: 'ongoing'},
-      {id: 2, name: 'done'},
-    ]
-  }
+  import store from './data'
 
   export default {
     name: 'app',
--- src/data.js
+++ src/data.js
@@ -2,10 +2,17 @@
  * Created by matbur on 7/23/17.
  */
 
-window.store = {
+export default {
   tasks: [
     {id: 0, text: 'task0', category: 0},
-    {id: 1, text: 'task1', category: 0}
+    {id: 1, text: 'task1', category: 0},
+    {id: 2, text: 'task2', category: 1},
+    {id: 3, text: 'task3', category: 2},
   ],
-  inp_data: ''
+  categories: [
+    {id: 0, name: 'backlog'},
+    {id: 1, name: 'ongoing'},
+    {id: 2, name: 'done'},
+  ]
 }
+
